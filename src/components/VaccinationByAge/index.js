4523b7683be38257bdb58be40bb0e30e4ba8252b
prashanth <reddyprashanth512@gmail.com>
2024-01-17 12:54:32 +0000

    solution

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,11 +1,19 @@
 // Write your code here
 import './index.css'
 
+import {BarChart, XAxis, YAxis, Legend, Bar} from 'recharts'
+
 const VaccinationByAge = props => {
   const {vaccinationByAge} = props
   return (
     <div className="card-container">
-      <p className="vaccination">Vaccination Coverage</p>
+      <p className="vaccination">Vaccination bg age</p>
+      <BarChart width={400} height={300} data={vaccinationByAge}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Legend />
+        <Bar dataKey="value" fill="#8884d8" />
+      </BarChart>
     </div>
   )
 }
--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,11 +1,19 @@
 // Write your code here
 import './index.css'
 
+import {BarChart, XAxis, YAxis, Legend, Bar} from 'recharts'
+
 const VaccinationByGender = props => {
   const {vaccinationByGender} = props
   return (
     <div className="card-container">
-      <p className="vaccination">Vaccination Coverage</p>
+      <p className="vaccination">Vaccination by gender</p>
+      <BarChart width={400} height={300} data={vaccinationByGender}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Legend />
+        <Bar dataKey="value" fill="#8884d8" />
+      </BarChart>
     </div>
   )
 }
